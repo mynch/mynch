mynch/mynch

commit 3e1b9ea331ec918175ee8f7e015dad17e6ee8b5b

Use Method::Signatures in Settings.pm

lib/Mynch/Settings.pm
@@ -22,28 +22,22 @@ use Mynch::Livestatus;
 use Quantum::Superpositions;
 use Method::Signatures;
 
-sub settings_page {
-    my $self = shift;
-
+method settings_page {
     $self->stash( settings => $self->session->{settings} );
     $self->render;
 }
 
-sub edit {
-    my $self = shift;
-
+method edit {
     $self->stash( settings => $self->session->{settings} );
     $self->render;
 }
 
-sub save_set {
-    my $self = shift;
-
+method save_set {
     my $set        = $self->param('set');
     my $label      = $self->param('label');
     my @hostgroups = $self->param('hostgroups');
 
-    my $hostgroups_ref = $self->wash_hostgroups( \@hostgroups );
+    my $hostgroups_ref = $self->wash_hostgroups( hostgroups => \@hostgroups );
 
     if ( $label and scalar @{$hostgroups_ref} ) {
         my $set_data = {
@@ -71,9 +65,7 @@ sub save_set {
     $self->redirect_to('/settings');
 }
 
-sub delete_set {
-    my $self = shift;
-
+method delete_set {
     my $set = $self->param('set');
 
     my $length = 1;    # Used for splicing
@@ -97,19 +89,13 @@ sub delete_set {
 
 }
 
-sub clear {
-    my $self = shift;
-
+method clear {
     $self->flash( message => 'settings reverted to default' );
     $self->session( expires => 1 );
     $self->redirect_to('/settings');
-
 }
 
-sub wash_hostgroups {
-    my $self       = shift;
-    my $hostgroups = shift;
-
+method wash_hostgroups ( ArrayRef $hostgroups ) {
     my @candidate_hostgroups = @{$hostgroups};
 
     my $live_hostgroups_ref = $self->list_hostgroups();
@@ -121,8 +107,7 @@ sub wash_hostgroups {
     return \@ok_hostgroups;
 }
 
-sub list_hostgroups {
-    my $self = shift;
+method list_hostgroups {
     my $ls = Mynch::Livestatus->new( config => $self->stash->{config}->{ml} );
 
     my $query;

commit 6a79f468e033095d4771d3070af7b1ed40950e8c

Fix bug introduced in 3e1b9ea3
* wash_hostgroups() does not take named parameters, just an array reference.

lib/Mynch/Settings.pm
@@ -37,7 +37,7 @@ method save_set {
     my $label      = $self->param('label');
     my @hostgroups = $self->param('hostgroups');
 
-    my $hostgroups_ref = $self->wash_hostgroups( hostgroups => \@hostgroups );
+    my $hostgroups_ref = $self->wash_hostgroups( \@hostgroups );
 
     if ( $label and scalar @{$hostgroups_ref} ) {
         my $set_data = {