a3b9a3fc76ba03e58ebf79001de6de594865ba5e
Erik Inge Bolsø <knan@redpill-linpro.com>
2012-08-22 12:14:20 +0200

    remote user - possible fix for #14

--- lib/Mynch/Dashboard.pm
+++ lib/Mynch/Dashboard.pm
@@ -319,7 +319,7 @@ sub dostuff {
     my $referrer = $self->req->headers->referrer;
 
     my $notify = 1;                                    # FIXME: adjustable
-    my $nick = $ENV{'REMOTE_USER'} || "vaktsmurfen";
+    my $nick = $self->req->env->{REMOTE_USER} || "vaktsmurfen";
 
     my $now = time();
 
